e77bf4dc36b3c991ddfd8f4b8af9b334171105ec
1914999 <1914999@01-19786.pusd.dom>
2023-05-17 10:57:38 -0700

    more things

--- Snake Game/src/Board.java
+++ Snake Game/src/Board.java
@@ -1,9 +1,11 @@
+import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Board extends JPanel implements KeyListener, ActionListener {
@@ -13,9 +15,17 @@ public class Board extends JPanel implements KeyListener, ActionListener {
 	
 	public void paint(Graphics g) {
 		super.paintComponent(g);
+		//food.paint(g) gotta paint the apple
 		
 		
 		
+		
+		
+		
+		
+	}
+	public static void man(String[] arg) {
+		Frame d = new Frame();
 	}
 
 
@@ -44,6 +54,12 @@ public class Board extends JPanel implements KeyListener, ActionListener {
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	public Board() {
+		JFrame f = new JFrame("Snake");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(800, 600);
 	}
 	
 	
