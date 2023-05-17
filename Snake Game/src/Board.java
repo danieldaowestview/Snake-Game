ea3ec48d6188074f24ad14628714155d9a62b3f8
1914999 <1914999@01-19786.pusd.dom>
2023-05-17 10:49:34 -0700

    Expanded on Board class

--- Snake Game/src/Board.java
+++ Snake Game/src/Board.java
@@ -1,14 +1,48 @@
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Board {
-	private int x;
-	private int y;
-	private int coords; 
+import javax.swing.JPanel;
+
+public class Board extends JPanel implements KeyListener, ActionListener {
+	private int height;
+	private int width;
 	
 	
-	public Board() {
+	public void paint(Graphics g) {
+		super.paintComponent(g);
 		
 		
 		
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 	
